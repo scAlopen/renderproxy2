18bde4121dea9e0ddbbb53489e7acdc37745f00b
JF <justinfady11@Gmail.com>
2025-06-09 14:37:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ const { createBareServer } = require("@tomphttp/bare-server-node");
 const app = express();
 const bare = createBareServer("/bare/");
 
-// Route bare client endpoints
 app.use((req, res, next) => {
   if (bare.shouldRoute(req)) return bare.routeRequest(req, res);
   next();
 });
 
-// Serve static UI assets
 app.use("/", express.static("ultraviolet-static"));
 
 const server = http.createServer(app);
 bare.attach(server);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`UV proxy running on port ${PORT}`));
+server.listen(PORT, () => console.log(`UV proxy running on ${PORT}`));
