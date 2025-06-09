589c9069606e5d84592ca06cf87bf18fd1bc1f10
JF <justinfady11@Gmail.com>
2025-06-08 21:04:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -39,5 +39,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(\`Proxy server listening on port \${PORT}\`);
+ console.log(`Proxy server listening on port ${PORT}`);
 });
